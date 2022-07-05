34fa819a61afc1a9962b4d9a1cb552325828eb3d
gabrielaalrocha <gabriela.leme95@hotmail.com>
2022-07-05 15:45:21 -0300

    #6 - criação dos repositories

---
+++ src/main/java/com/trofeuCaetano/Repositories/ApontamentoRepository.java
@@ -0,0 +1,7 @@
+package com.trofeuCaetano.Repositories;
+
+import com.trofeuCaetano.Models.Apontamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApontamentoRepository extends JpaRepository<Apontamento, Integer> {
+}
